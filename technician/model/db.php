31bdd4099d11a368a38fb4999e8c4a190e3d1900
Md Tamjid Hossain <32460180+MdTamjidHossain@users.noreply.github.com>
2025-01-29 11:44:12 -0500

    Add files via upload

--- technician/model/db.php
+++ technician/model/db.php
@@ -35,6 +35,21 @@ class myDB
         return $results;
     }
 
+    function getUserInfo($connectionObject, $tech_id)
+    {
+        $sql = "SELECT * FROM technician WHERE technician_id=$tech_id";
+        $result = $connectionObject->query($sql);
+        return $result->fetch_assoc();
+    }
+
+    function updatePassword($connectionObject, $tech_id, $user_id, $newPassword) {
+        $sql_technician = "UPDATE technician SET password='$newPassword' WHERE technician_id = '$tech_id' ";
+        $technician_result = $connectionObject->query($sql_technician);
+        $sql_user = "UPDATE user SET password='$newPassword' WHERE user_id = '$user_id' ";
+        $user_result = $connectionObject->query($sql_user);
+        return ($technician_result && $user_result);
+    }
+
     function viewUpcomingAppointments($technician_id, $connectionobject)
     {
         $sql = "SELECT a.appointment_id, a.appointment_date, a.status, c.name AS customer_name, c.phone AS customer_phone 
@@ -132,6 +147,8 @@ class myDB
 
 
 
+
+
     public function updateCustomerRating($connectionObject, $appointment_id, $rating, $comment)
     {
 
@@ -141,7 +158,6 @@ class myDB
         WHERE appointment_id = '$appointment_id'
     ";
 
-        // Execute the query and return the result
         if ($connectionObject->query($sql)) {
             return true;
         } else {
@@ -150,12 +166,89 @@ class myDB
     }
 
 
+    public function getCustomerFeedback($connectionObject, $tech_id)
+    {
+        $sql = "SELECT a.customer_id, c.name, c.email, c.phone, a.status AS appointment_status, a.customer_rating, a.customer_comment, a.appointment_id
+            FROM appointment a
+            JOIN customer c ON a.customer_id = c.customer_id
+            WHERE a.technician_id = '$tech_id' AND a.status = 'Completed';";
+
+
+        $result = $connectionObject->query($sql);
+
+        if ($result && $result->num_rows > 0) {
+            $feedback = [];
+            while ($row = $result->fetch_assoc()) {
+                $feedback[] = $row;
+            }
+            return $feedback;
+        }
+        return [];
+    }
+
+    function totalAppointmentByTechnicianID($connectionObject, $tech_id)
+    {
+        $sql = "SELECT technician_id, COUNT(*) AS total_appointments FROM appointment WHERE technician_id = $tech_id GROUP BY technician_id;";
+        $result = $connectionObject->query($sql);
+        return $result;
+    }
+
+    function pendingAppointmentByTechnicianID($connectionObject, $tech_id)
+    {
+        $sql = "SELECT technician_id, COUNT(*) AS pending_appointments FROM appointment WHERE technician_id = $tech_id AND status = 'Pending' GROUP BY technician_id;";
+        $result = $connectionObject->query($sql);
+        return $result;
+    }
+
+    function completedAppointmentByTechnicianID($connectionObject, $tech_id)
+    {
+        $sql = "SELECT technician_id, COUNT(*) AS completed_appointments FROM appointment WHERE technician_id = $tech_id AND status = 'Completed' GROUP BY technician_id;";
+        $result = $connectionObject->query($sql);
+        return $result;
+    }
+
+    function totalReviewsByTechnicianID($connectionObject, $tech_id)
+    {
+        $sql = "SELECT technician_id, COUNT(*) AS total_reviews FROM appointment WHERE technician_id = $tech_id AND customer_rating != 0 GROUP BY technician_id;";
+        $result = $connectionObject->query($sql);
+        return $result;
+    }
 
 
     function getUserByID($table, $connectionObject, $id)
     {
         $sql = 'SELECT * FROM $table WHERE id=$id';
     }
+
+    function insertMessage($email, $subject, $message, $connectionObject)
+    {
+        $sql = "SELECT user_type FROM user WHERE email = '$email'";
+        $result = $connectionObject->query($sql);
+
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $senderType = $row['user_type'];
+            $insertSql = "INSERT INTO messages (email, subject, message, sent_date,user_type) 
+                      VALUES ('$email', '$subject', '$message',  NOW(),'$senderType')";
+            return $connectionObject->query($insertSql);
+        } else {
+            $insertSql = "INSERT INTO messages (email, subject, message, sent_date,user_type) 
+                      VALUES ('$email', '$subject', '$message',  NOW(),'Guest')";
+            return $connectionObject->query($insertSql);
+        }
+    }
+
+
+    function insertReport($technician_id, $subject, $priority, $message, $connectionObject)
+    {
+
+        $insertSql = "INSERT INTO reports (technician_id, subject, priority, message , sent_date) 
+                      VALUES ('$technician_id', '$subject', '$priority',  '$message',NOW())";
+        return $connectionObject->query($insertSql);
+    }
+
+
+
     function closecon($connectionObject)
     {
         $connectionObject->close();
